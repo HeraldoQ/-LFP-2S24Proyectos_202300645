740782406d243dc0efccca159318df3b1d79b896
Heraldo <3084109550404@ingenieria.usac.edu.gt>
2024-09-07 13:27:56 -0600

    no se

---
+++ Proyecto1/Proyecto1.f90
@@ -0,0 +1,5 @@
+program Proyecto1
+    implicit none
+    print*, "holacdsvfbngfbdscadvfbngmnfbdvscdvbfngmhgnfbdvsbfngmhmgnfdsvfgnhmj,gfbdvsbfcdsvbfvvdbcsvfdbgdvdsdfgfnbdvsbgfnbdvsbfgngmh,mgnfbdvsbfngmhgnfbd"
+    
+end program Proyecto1
